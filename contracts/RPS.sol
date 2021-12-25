75ed08d7c13926894f61ae2d9ff059a012174748
VMasfer <93734576+VMasfer@users.noreply.github.com>
2021-12-25 01:42:22 -0300

    feat: created interface IRPS.sol

--- contracts/RPS.sol
+++ contracts/RPS.sol
@@ -1,10 +1,11 @@
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
+import './IRPS.sol';
 
 //SPDX-License-Identifier: Unlicense
 pragma solidity >=0.8.4 <0.9.0;
 
-contract RPS is ERC20, Ownable {
+contract RPS is IRPS, ERC20, Ownable {
   address public rockPaperScissors;
 
   constructor(address _rockPaperScissors) ERC20('RockPaperScissors Token', 'RPS') {
@@ -15,13 +16,13 @@ contract RPS is ERC20, Ownable {
     rockPaperScissors = _rockPaperScissors;
   }
 
-  function mint(address _to, uint256 _amount) external {
+  function mint(address _to, uint256 _amount) external override {
     //solhint-disable-next-line
     require(msg.sender == rockPaperScissors, 'Only the RockPaperScissors contract can call this function');
     _mint(_to, _amount);
   }
 
-  function burn(address _from, uint256 _amount) external {
+  function burn(address _from, uint256 _amount) external override {
     //solhint-disable-next-line
     require(msg.sender == rockPaperScissors, 'Only the RockPaperScissors contract can call this function');
     _burn(_from, _amount);
